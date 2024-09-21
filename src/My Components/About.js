f34338f4204e3cdf6f80da07fc2585a2dae7b018
AfzalAtiq <122719950+AfzalAtiq@users.noreply.github.com>
2024-09-21 14:00:18 +0500

    Initial commit

---
+++ src/My Components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        This is an about Component
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia vehicula diam, at faucibus metus vulputate non. Maecenas ut ante tincidunt, faucibus urna nec, ultricies est. Duis consequat, risus at tincidunt vestibulum, elit purus convallis dolor, nec varius justo lectus vel urna.
+        </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/My Components/Todos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {TodoItem} from "./TodoItem";
+export const Todos = (props) => {
+  let mystyle ={
+    minheight: "70vh",
+    margin: "10 px auto " 
+  }
+    return (
+    <div className="container my-3 " style={mystyle}>
+        <h3 className=" my-3 ">Todos list</h3>
+        {props.todos.length==0? "No Todos to display":
+        props.todos.map((todo)=>{
+          return( <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/> )
+          })
+        }
+    </div>
+
+  )
+}
+
+export default Todos
